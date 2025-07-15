b732504b12ae6ea0cea6e32c3b5770da1c36c1ee
HasalKenula <hasalkenula255@gmail.com>
2025-07-15 14:08:22 +0530

    add the login and register button

--- Gym_Management_System/Form1.Designer.cs
+++ Gym_Management_System/Form1.Designer.cs
@@ -29,23 +29,45 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(142, 158);
+            this.label1.Location = new System.Drawing.Point(133, 52);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(577, 54);
             this.label1.TabIndex = 0;
             this.label1.Text = "Gym Management System";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 287);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(330, 347);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Register";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +79,8 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
