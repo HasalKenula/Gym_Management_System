5b97ade18b013e364905c381067e061cb80e406f
HasalKenula <hasalkenula255@gmail.com>
2025-07-18 23:42:27 +0530

    created login page interface

--- Gym_Management_System/Form1.cs
+++ Gym_Management_System/Form1.cs
@@ -15,6 +15,50 @@ namespace Gym_Management_System
         public Form1()
         {
             InitializeComponent();
+
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetRoundedButton(button1, 40);
+            SetRoundedButton(button2, 30);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.BackColor = Color.DodgerBlue;
+            button1.ForeColor = Color.White;
+
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.BackColor = Color.DodgerBlue;
+            button2.ForeColor = Color.White;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void SetRoundedButton(Button button, int radius)
+        {
+            System.Drawing.Drawing2D.GraphicsPath path = new System.Drawing.Drawing2D.GraphicsPath();
+            Rectangle rect = button.ClientRectangle;
+            path.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
+            path.AddArc(rect.Right - radius, rect.Y, radius, radius, 270, 90);
+            path.AddArc(rect.Right - radius, rect.Bottom - radius, radius, radius, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - radius, radius, radius, 90, 90);
+            path.CloseAllFigures();
+            button.Region = new Region(path);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
