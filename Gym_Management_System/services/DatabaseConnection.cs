87e76c2605fb5419835f76e907b4110847929cc7
Isira Hansaja <153202522+iSiRaH@users.noreply.github.com>
2025-07-30 20:14:22 +0530

    fix errors

--- Gym_Management_System/services/DatabaseConnection.cs
+++ Gym_Management_System/services/DatabaseConnection.cs
@@ -11,7 +11,7 @@ namespace Gym_Management_System.services
     {
         private static DatabaseConnection _instance;
         private static readonly object _lock = new object();
-        private readonly string _connectionString = @"Data Source=DESKTOP-UTSQ2RQ\SQLEXPRESS;Initial Catalog=student;Integrated Security=True";
+        private readonly string _connectionString = @"Data Source=DESKTOP-8T2OQ4F;Initial Catalog=student;Integrated Security=True;Trust Server Certificate=True";
         private SqlConnection _connection;
 
       
