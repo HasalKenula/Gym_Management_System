a387d2440492ef4e5cf8ae25ad059a7f28301155
HasalKenula <hasalkenula255@gmail.com>
2025-10-27 16:45:12 +0530

    created a attendence UI

--- Gym_Management_System/pages/admin/AttendanceManegement.Designer.cs
+++ Gym_Management_System/pages/admin/AttendanceManegement.Designer.cs
@@ -29,23 +29,210 @@
         private void InitializeComponent()
         {
             this.pnlAttendanceManage = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.pnlAttendanceManage.SuspendLayout();
             this.SuspendLayout();
             // 
             // pnlAttendanceManage
             // 
+            this.pnlAttendanceManage.BackColor = System.Drawing.Color.MidnightBlue;
+            this.pnlAttendanceManage.Controls.Add(this.comboBox2);
+            this.pnlAttendanceManage.Controls.Add(this.comboBox1);
+            this.pnlAttendanceManage.Controls.Add(this.textBox5);
+            this.pnlAttendanceManage.Controls.Add(this.textBox4);
+            this.pnlAttendanceManage.Controls.Add(this.textBox3);
+            this.pnlAttendanceManage.Controls.Add(this.textBox2);
+            this.pnlAttendanceManage.Controls.Add(this.textBox1);
+            this.pnlAttendanceManage.Controls.Add(this.label8);
+            this.pnlAttendanceManage.Controls.Add(this.label7);
+            this.pnlAttendanceManage.Controls.Add(this.label6);
+            this.pnlAttendanceManage.Controls.Add(this.label5);
+            this.pnlAttendanceManage.Controls.Add(this.label4);
+            this.pnlAttendanceManage.Controls.Add(this.label3);
+            this.pnlAttendanceManage.Controls.Add(this.label2);
+            this.pnlAttendanceManage.Controls.Add(this.label1);
             this.pnlAttendanceManage.Location = new System.Drawing.Point(-2, -1);
             this.pnlAttendanceManage.Name = "pnlAttendanceManage";
-            this.pnlAttendanceManage.Size = new System.Drawing.Size(805, 450);
+            this.pnlAttendanceManage.Size = new System.Drawing.Size(1501, 888);
             this.pnlAttendanceManage.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(513, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(539, 54);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Attendence Mangement";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(53, 134);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Attendance_id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label3.Location = new System.Drawing.Point(53, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(164, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Member_id";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label4.Location = new System.Drawing.Point(53, 294);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(197, 32);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Member_type";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label5.Location = new System.Drawing.Point(53, 376);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 32);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Date";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label6.Location = new System.Drawing.Point(842, 134);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(214, 32);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Check_in_time";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label7.Location = new System.Drawing.Point(842, 209);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(232, 32);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Check_out_time";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label8.Location = new System.Drawing.Point(842, 294);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(101, 32);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Status";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(306, 134);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(274, 38);
+            this.textBox1.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(306, 209);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(274, 38);
+            this.textBox2.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(306, 370);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(274, 38);
+            this.textBox3.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(1165, 128);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(274, 38);
+            this.textBox4.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(1165, 203);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(274, 38);
+            this.textBox5.TabIndex = 13;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Trainer",
+            "Player"});
+            this.comboBox1.Location = new System.Drawing.Point(309, 294);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(270, 39);
+            this.comboBox1.TabIndex = 14;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Absent",
+            "Present",
+            "Late"});
+            this.comboBox2.Location = new System.Drawing.Point(1165, 294);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(270, 39);
+            this.comboBox2.TabIndex = 15;
             // 
             // AttendanceManegement
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1482, 873);
             this.Controls.Add(this.pnlAttendanceManage);
             this.Name = "AttendanceManegement";
             this.Text = "AttendanceManegement";
+            this.pnlAttendanceManage.ResumeLayout(false);
+            this.pnlAttendanceManage.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -53,5 +240,20 @@
         #endregion
 
         private System.Windows.Forms.Panel pnlAttendanceManage;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- Gym_Management_System/pages/admin/AttendanceManegement.cs
+++ Gym_Management_System/pages/admin/AttendanceManegement.cs
@@ -21,5 +21,10 @@ namespace Gym_Management_System.pages.admin
         {
             return pnlAttendanceManage;
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
